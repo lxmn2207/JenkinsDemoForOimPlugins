c528a2b25661782c9a46583d366560ff578e0023
lxmn2207 <bhaleraoy1@gmail.com>
2022-09-18 14:39:36 +0530

    update file

---
+++ src/my_code/MyCode.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+package my_code;
+
+public class Tets {
+	
+	
+	public static void main(String[] args) {
+		
+
+		System.out.println("This is first java jar by jenkins!");
+	
+}
+
+}
